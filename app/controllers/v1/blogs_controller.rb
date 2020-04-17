70fa976b3c685c1f34bd97bf1a62a36adf18e287
TravelingTice <matthijs29@live.com>
2020-04-17 20:44:01 +0200

    Add create action to blogs + add validations blog

--- app/controllers/v1/blogs_controller.rb
+++ app/controllers/v1/blogs_controller.rb
@@ -1,2 +1,14 @@
 class V1::BlogsController < ApplicationController
+  def create
+    @blog = Blog.new(blog_params)
+    if !@blog.save
+      return render json: { error: @blog.errors.full_messages.first }
+    end
+  end
+
+  private
+
+  def blog_params
+    params.permit(:title, :body, :categories, :tags, :photo)
+  end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
 
+  has_one_base64_attached :photo
+
   validates :title, :slug, :body, presence: true
   validates :title, length: { minimum: 49, maximum: 61 }
 end
