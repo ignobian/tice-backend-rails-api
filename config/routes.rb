ea56d00b59da607fa747b6e6498b2c6b31e60906
TravelingTice <matthijs29@live.com>
2020-04-17 11:40:43 +0200

    Add routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     get 'wake-up', to: 'pages#wake_up'
+
+    resources :sessions, only: [:create]
+    resources :registrations, only: [:index, :show, :create, :update, :destroy] do
+      collection do
+        post 'pre-signup', to: 'registrations#pre_signup'
+        post 'invite', to: 'registrations#invite'
+        post 'signup-from-invite', to: 'registrations#create_on_invite'
+        put 'forgot-password', to: 'registrations#forgot_password'
+        put 'reset-password', to: 'registrations#reset_password'
+      end
+    end
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
