4a2f7ef7888272a4313a126d203f03fbc451f737
TravelingTice <matthijs29@live.com>
2020-04-17 20:34:15 +0200

    add simple validations to blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ApplicationRecord
+
+  validates :title, :slug, :body, presence: true
+  validates :title, length: { minimum: 49, maximum: 61 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
         delete ':slug', to: 'categories#destroy'
       end
     end
+
+    resources :blogs, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
