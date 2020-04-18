79fa50428f1f169dc3b26df962965006210b5226
TravelingTice <matthijs29@live.com>
2020-04-18 13:33:01 +0200

    Add report types

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,13 @@
 class Report < ApplicationRecord
   belongs_to :user
   belongs_to :blog
+
+  REPORT_TYPES = [
+    'Sexual content',
+    'Violent content',
+    'Hateful content',
+    'Spam',
+    'Copyright content'
+  ]
+
 end
