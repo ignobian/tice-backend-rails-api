1ddc251ef301293cf225ded2947a390ae9aac37b
TravelingTice <matthijs29@live.com>
2020-04-18 12:53:06 +0200

    Add validation for uniqueness of share

--- app/models/share.rb
+++ app/models/share.rb
@@ -5,4 +5,5 @@ class Share < ApplicationRecord
   enum type: %i(twitter linkedin facebook)
 
   validates :type, presence: true
+  validates :user, uniqueness: { scope: [:blog, :type], message: 'You already shared this post with this type' }
 end
