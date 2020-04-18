3c5d73195aecb1580218b3b5e1b58c54e14c5c62
TravelingTice <matthijs29@live.com>
2020-04-18 17:17:42 +0200

    Add report params, add report#create

--- app/controllers/v1/reports_controller.rb
+++ app/controllers/v1/reports_controller.rb
@@ -1,5 +1,19 @@
 class V1::ReportsController < ApplicationController
+  before_action :auth_required
+
   def create
-    byebug
+    @report = Report.new(report_params)
+    @blog = Blog.find(params[:blog_id])
+    @report.user = @user
+    @report.blog = @blog
+    if !@report.save
+      return render json: { error: @report.errors.full_messages.first }
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:name)
   end
 end
