9cf98d6f137bdc450d3079086af485c0ef5c8abf
TravelingTice <matthijs29@live.com>
2020-04-18 13:10:06 +0200

    Add impressions controller

---
+++ app/controllers/v1/impressions_controller.rb
@@ -0,0 +1,35 @@
+class V1::ImpressionsController < ApplicationController
+  before_action :auth_required, only: [:create]
+
+  def create
+    @impression = Impression.new(impression_params)
+    @impression.user = @user
+
+    if !@impression.save
+      return render json: { message: @impression.errors.full_messages.first }
+    end
+  end
+
+  def add_not_signed_in
+    # check if impression doesnt exist with user agent
+    @blog = Blog.find(impression_params[:blog_id])
+    user_agent = request.headers['user-agent']
+
+    found = Impression.find_by(blog: @blog, user_agent: user_agent)
+    unless found.nil?
+      return render json: { message: 'This user agent already impressiond this post this way' }
+    end
+
+    # add the user agent
+    @impression = Impression.new(blog: @blog, user_agent: user_agent)
+    if !@impression.save
+      return render json: { message: @impression.errors.full_messages.first }
+    end
+  end
+
+  private
+
+  def impression_params
+    params.require(:impression).permit(:blog_id)
+  end
+end
