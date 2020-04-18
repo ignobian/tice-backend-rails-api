b3d9a02f5c3390fcd10ef23c119178271e061813
TravelingTice <matthijs29@live.com>
2020-04-18 12:50:55 +0200

    Add create action for share, add validations

--- app/controllers/v1/shares_controller.rb
+++ app/controllers/v1/shares_controller.rb
@@ -1,8 +1,20 @@
 class V1::SharesController < ApplicationController
   def create
+    @share = Share.new(share_params)
+    @share.user = @user
+
+    if !@share.save
+      return render json: { error: @share.errors.full_messages.first }
+    end
   end
 
   def add_not_signed_in
 
+  end
+
+  private
+
+  def share_params
+    params.require(:share).permit(:type, :blog_id)
   end
 end
--- app/models/share.rb
+++ app/models/share.rb
@@ -1,4 +1,8 @@
 class Share < ApplicationRecord
   belongs_to :user
   belongs_to :blog
+
+  enum type: %i(twitter linkedin facebook)
+
+  validates :type, presence: true
 end
