f2df692f64d926708ac202655beba30f44bb718a
TravelingTice <matthijs29@live.com>
2020-04-18 19:21:19 +0200

    Add route for reports#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :reports, only: [:index, :create]
+    resources :reports, only: [:index, :create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
