f0e0c47be51bfa39bd0d65104394f88d70670d89
TravelingTice <matthijs29@live.com>
2020-04-19 21:06:43 +0200

    Add contact mailer action

--- app/mailers/author_mailer.rb
+++ app/mailers/author_mailer.rb
@@ -1,2 +1,10 @@
 class AuthorMailer < ApplicationMailer
+  def contact
+    @from_user = params[:from_user]
+    @author = params[:author]
+    @subject = params[:subject]
+    @message = params[:message]
+
+    mail(to: @author.email, from: @from_user.email, subject: "#{@from_user.name} on #{ENV['APP_NAME']}: #{@subject}")
+  end
 end
