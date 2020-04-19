904c11b03fcbf93343b0840cfeeaea12da715a1b
TravelingTice <matthijs29@live.com>
2020-04-19 20:33:50 +0200

    Add toggle_follower logic and view

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -17,6 +17,19 @@ class V1::UsersController < ApplicationController
     @user.photo.attach(data: params[:photo], filename: 'avatar.jpg', content_type: 'image/jpg') if params[:photo].present?
   end
 
+  def toggle_follower
+    @to_be_un_followed = User.find(params[:id])
+    # add follower
+    if !@to_be_un_followed.followers.where(id: @user.id).empty?
+      # remove follower
+      Following.where(user: @to_be_un_followed, follower: @user).destroy_all
+    else
+      # add follower
+      Following.create(user: @to_be_un_followed, follower: @user)
+    end
+    # @user.followings << @to_be_un_followed
+  end
+
   private
 
   def user_params
---
+++ app/views/v1/users/toggle_follower.json.jbuilder
@@ -0,0 +1,1 @@
+json.message 'Follower added'
