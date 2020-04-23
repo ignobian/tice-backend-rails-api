0a3b16a4ffd541784b2a2666310b8a8b1e55299a
TravelingTice <matthijs29@live.com>
2020-04-23 13:16:07 +0200

    Migrate and add small validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_113116) do
+ActiveRecord::Schema.define(version: 2020_04_23_111536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,16 @@ ActiveRecord::Schema.define(version: 2020_04_18_113116) do
     t.datetime "updated_at", null: false
     t.index ["blog_id"], name: "index_claps_on_blog_id"
     t.index ["user_id"], name: "index_claps_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.bigint "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "followings", force: :cascade do |t|
@@ -150,6 +160,8 @@ ActiveRecord::Schema.define(version: 2020_04_18_113116) do
   add_foreign_key "blogs", "users"
   add_foreign_key "claps", "blogs"
   add_foreign_key "claps", "users"
+  add_foreign_key "comments", "blogs"
+  add_foreign_key "comments", "users"
   add_foreign_key "impressions", "blogs"
   add_foreign_key "impressions", "users"
   add_foreign_key "reports", "blogs"
