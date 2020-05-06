88502ebc4efbdaa76cb05ca8a835ff3ac3a90b88
TravelingTice <matthijs29@live.com>
2020-05-06 12:55:59 +0200

    Add view for index-xml with category last modified helper

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
   has_many :blogs, through: :blog_categories
 
   validates :name, :slug, presence: true
+
+  def last_modified
+    blogs.order('updated_at DESC').last.updated_at
+  end
 end
---
+++ app/views/v1/pages/index_xml.json.jbuilder
@@ -0,0 +1,7 @@
+json.last_mod @last_mod
+
+json.categories do
+  json.array! @categories do |cat|
+    json.(cat, :slug, :last_modified)
+  end
+end
