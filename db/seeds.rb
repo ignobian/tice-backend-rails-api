2cfb2e12dab5265d359d389179e57658a62bf63f
TravelingTice <matthijs29@live.com>
2020-05-08 16:57:10 +0200

    Add seeder for cloudinary account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(password: 'password', username: 'admin01', role: 'admin', first_name: 'Mr', last_name: 'Admin', email: 'admin@example.com')
-Category.create!(name: 'Next Js', slug: 'next-js')
-Category.create!(name: 'React Js', slug: 'react-js')
-Category.create!(name: 'Node Js', slug: 'node-js')
+# User.create!(password: 'password', username: 'admin01', role: 'admin', first_name: 'Mr', last_name: 'Admin', email: 'admin@example.com')
+# Category.create!(name: 'Next Js', slug: 'next-js')
+# Category.create!(name: 'React Js', slug: 'react-js')
+# Category.create!(name: 'Node Js', slug: 'node-js')
+
+User.all.each do |user|
+  if user.photo.attached?
+    Cloudinary::Uploader.upload("http://res.cloudinary.com/ticekralt/image/upload/#{user.photo.key}", public_id: user.photo.key)
+  end
+end
+
+Blog.all.each do |blog|
+  if blog.photo.attached?
+    Cloudinary::Uploader.upload("http://res.cloudinary.com/ticekralt/image/upload/#{blog.photo.key}", public_id: blog.photo.key)
+  end
+
+end
