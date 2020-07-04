527812744f8cfae733ba3ce83bfc1f3bd1f2a69d
TravelingTice <matthijs29@live.com>
2020-07-04 15:58:09 +0200

    Add updated auth protection methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,25 +2,34 @@ class ApplicationController < ActionController::API
   # some helper methods for protecting routes
   # for this action you need to be signed in
   def auth_required
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
-    begin
-      data = JWT.decode(header, ENV['JWT_SECRET'])[0]
-
-      @user = User.not_deleted.find(data['user_id'])
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Not authorized to perform this action' }, status: :unauthorized
-    rescue JWT::DecodeError => e
-      render json: { error: 'Not authorized to perform this action' }, status: :unauthorized
+    unless is_auth?
+      return render json: { error: 'Not authorized to perform this action'}, status: :unauthorized
     end
   end
 
   # for this action you need to be admin
   def admin_required
-    auth_required
-
-    unless @user.admin?
-      render json: { error: 'Admin resource, access denied' }, status: :unauthorized
+    if !is_auth? || !@user.admin?
+      return render json: { error: 'Not authorized to perform this action' }, status: :unauthorized
     end
+  end
+
+  private
+
+  def is_auth?
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+
+    begin
+      data = JWT.decode(header, ENV['JWT_SECRET'])[0]
+
+      @user = User.find(data['user_id'])
+    rescue ActiveRecord::RecordNotFound
+      return false
+    rescue JWT::DecodeError => e
+      return false
+    end
+
+    true
   end
 end
