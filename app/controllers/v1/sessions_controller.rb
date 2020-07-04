0835798c8b5e75a48bf3e1ef3c6b0d5cc7911157
TravelingTice <matthijs29@live.com>
2020-07-04 16:39:44 +0200

    Add login tests

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,15 +1,19 @@
 class V1::SessionsController < ApplicationController
   def create
+    if !Devise.email_regexp.match?(session_params[:email])
+      return render json: { error: 'Please enter a valid email' }, status: :bad_request
+    end
+
     @user = User.not_deleted.find_by(email: session_params[:email])
 
     if !@user
-      return render json: { error: "User with email #{session_params[:email]} not found. Please sign up." }
+      return render json: { error: "User with email #{session_params[:email]} not found. Please sign up." }, status: :not_found
     end
 
     if @user&.valid_password?(session_params[:password])
       @token = @user.generate_jwt
     else
-      render json: { error: 'Email and password did not match' }, status: :unauthorized
+      render json: { error: 'Email and password did not match' }, status: :bad_request
     end
   end
 
--- test/controllers/v1/sessions_controller_test.rb
+++ test/controllers/v1/sessions_controller_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class V1::SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create(username: 'Mattieman', first_name: 'Matthew', email: 'matt@example.com', last_name: 'Portland', password: 'Password123', password_confirmation: 'Password123')
+  end
+
+  test "Login" do
+    new_login = {
+      email: 'matt@example.com',
+      password: 'Password123'
+    }
+
+    post v1_sessions_path, params: { session: new_login }
+    assert_response :success
+    parsed_response = JSON.parse(response.body)
+    user_id_from_token = JWT.decode(parsed_response['token'], ENV['JWT_SECRET'], 'HS256').first['user_id']
+    assert_equal(@user.id, user_id_from_token)
+  end
+
+  test "Invalid email" do
+    invalid_login = {
+      email: 'verybademail',
+      password: 'Password123'
+    }
+
+    post v1_sessions_path, params: { session: invalid_login }
+    assert_response :bad_request
+    parsed_response = JSON.parse(response.body)
+    assert_match(/Please enter a valid email/, parsed_response['error'])
+  end
+
+  test "Invalid password" do
+    invalid_password = {
+      email: 'matt@example.com',
+      password: 'notmypassword'
+    }
+    post v1_sessions_path, params: { session: invalid_password }
+    assert_response :bad_request
+    parsed_response = JSON.parse(response.body)
+    assert_match(/Email and password did not match/, parsed_response['error'])
+  end
+
+  test "Email that is not in db" do
+    foreign_email = {
+      email: 'thisonedoesntexist@example.com',
+      password: 'mysecretpassword'
+    }
+    post v1_sessions_path, params: { session: foreign_email }
+    assert_response :not_found
+    parsed_response = JSON.parse(response.body)
+    assert_match(/User with email thisonedoesntexist@example.com not found. Please sign up/, parsed_response['error'])
+  end
 end
